e9869ecfbb7a9b7b742e3071b90dede8ae6b3d87
anzwkk <anam89737@gmail.com>
2024-12-16 22:57:53 +0300

    Добавьте файлы проекта.

---
+++ OnlineOrder/Program.cs
@@ -0,0 +1,64 @@
+﻿int orderNumber;
+string name;
+string productName;
+double productPrice;
+string addressName;
+int addressNum;
+
+Console.WriteLine("Введите номер заказа: ");
+orderNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите имя: ");
+name = Console.ReadLine();
+Console.WriteLine("Введите название товара: ");
+productName = Console.ReadLine();
+Console.WriteLine("Введите цену: ");
+productPrice = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите улицу : ");
+addressName = Console.ReadLine();
+Console.WriteLine("Введите номер адреса : ");
+addressNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер заказа: ");
+orderNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите имя: ");
+name = Console.ReadLine();
+Console.WriteLine("Введите название товара: ");
+productName = Console.ReadLine();
+Console.WriteLine("Введите цену: ");
+productPrice = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите улицу : ");
+addressName = Console.ReadLine();
+Console.WriteLine("Введите номер адреса : ");
+addressNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер заказа: ");
+orderNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите имя: ");
+name = Console.ReadLine();
+Console.WriteLine("Введите название товара: ");
+productName = Console.ReadLine();
+Console.WriteLine("Введите цену: ");
+productPrice = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите улицу : ");
+addressName = Console.ReadLine();
+Console.WriteLine("Введите номер адреса : ");
+addressNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Order No {orderNumber} ");
+Console.WriteLine($"Client: {name}");
+Console.WriteLine($"Product: {productName}, price {productPrice} EUR ");
+Console.WriteLine($"Address: {addressName}, {addressNum}");
+
+Console.WriteLine($"Order No {orderNumber} ");
+Console.WriteLine($"Client: {name}");
+Console.WriteLine($"Product: {productName}, price {productPrice} EUR ");
+Console.WriteLine($"Address: {addressName}, {addressNum}");
+
+Console.WriteLine($"Order No {orderNumber} ");
+Console.WriteLine($"Client: {name}");
+Console.WriteLine($"Product: {productName}, price {productPrice} EUR ");
+Console.WriteLine($"Address: {addressName}, {addressNum}");
+
+
+
+
